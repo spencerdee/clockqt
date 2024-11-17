7f471b1507d34f52c344c5dbee840ea116e74664
spencerdee <spencerdee9406@gmail.com>
2024-11-17 18:33:33 -0500

    linux windows support

--- src/Common/common.cpp
+++ src/Common/common.cpp
@@ -3,17 +3,21 @@
 /**
  * Windows
  */
-// void getCurrentTime(tm* tm)
-// {
-//     std::time_t now = std::time(0);
-//     localtime_s(tm, &now);
-// }
+#ifdef _WIN32
+void getCurrentTime(tm* tm)
+{
+    std::time_t now = std::time(0);
+    localtime_s(tm, &now);
+}
+#endif
 
 /**
  * Linux
  */
+#ifdef __linux__
 void getCurrentTime(tm* tm)
 {
     std::time_t now = std::time(0);
     localtime_r(&now, tm);
 }
+#endif
